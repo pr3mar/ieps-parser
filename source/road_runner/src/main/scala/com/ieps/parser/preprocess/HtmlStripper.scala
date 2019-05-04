d19e60d429bad373aaddd62ccb63bdac16c85b08
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-05-04 18:46:38 +0200

    progress

--- source/road_runner/src/main/scala/com/ieps/parser/preprocess/HtmlStripper.scala
+++ source/road_runner/src/main/scala/com/ieps/parser/preprocess/HtmlStripper.scala
@@ -2,11 +2,20 @@ package com.ieps.parser.preprocess
 
 import org.jsoup.nodes.Document
 import org.jsoup.safety.{Cleaner, Whitelist}
+import scala.collection.JavaConverters._
 
 object HtmlStripper {
   def strip(document: Document): Document = {
+    // remove iframe, head, script tags
     document.select("head,script,input,map").remove()
     val whitelist: Whitelist = Whitelist.relaxed()
-    new Cleaner(whitelist).clean(document)
+    // remove tag attributes
+    val cleanedDocument: Document = new Cleaner(whitelist).clean(document)
+    // remove empty tags such as `<span></span>` and nested tags with no text `<div><div></div></div>`
+    // adapted from here: https://stackoverflow.com/questions/8711032/remove-empty-tag-pairs-from-html-fragment#8803252
+    cleanedDocument.select("*").asScala.foreach { element =>
+      if ((!element.hasText && element.isBlock) || (!element.hasText && element.childNodes().isEmpty)) element.remove()
+    }
+    cleanedDocument
   }
 }
--- source/road_runner/src/main/scala/com/ieps/parser/roadRunner/RoadRunner.scala
+++ source/road_runner/src/main/scala/com/ieps/parser/roadRunner/RoadRunner.scala
@@ -9,13 +9,22 @@ import com.typesafe.scalalogging.StrictLogging
 import org.jsoup.nodes.{Document, Element}
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 import scala.util.Random
 
 object RoadRunner {
   def apply(inputDocuments: List[Document]): RoadRunner = new RoadRunner(inputDocuments)
+
+  trait Mismatch
+  case object TagMismatch extends Mismatch
+  case object LengthMismatch extends Mismatch
+  case object OptionMismatch extends Mismatch
+  case object TextMismatch extends Mismatch
 }
 
 class RoadRunner(inputDocuments: List[Document]) extends StrictLogging {
+  import RoadRunner._
+
   if (inputDocuments.isEmpty) {
     throw new Exception("Document list cannot be empty.")
   }
@@ -37,48 +46,88 @@ class RoadRunner(inputDocuments: List[Document]) extends StrictLogging {
     }
   }
 
-  def handleEmptyChildren(rootWrapper: Element, rootDocument: Element): Boolean = {
+  private def compareTag(wrapper: Element, document: Element): Boolean = wrapper.tagName().equals(document.tagName())
+
+  private def compareText(rootWrapper: Element, rootDocument: Element): Option[Mismatch] = {
     val wrapperText = rootWrapper.text()
     val documentText = rootDocument.text()
     if (!wrapperText.equals(documentText)) {
-      logger.info(s"Content mismatch: `$wrapperText` : `$documentText`")
-      false
+      Some(TextMismatch)
     } else {
-      true
+      None
     }
   }
 
-  private def expandWrapper(rootWrapper: Element, rootDocument: Element): Element = {
+  private def compareNodes(reference: Element, document: Element): List[Mismatch] = {
+    var mismatch: List[Mismatch] = Nil
+    if (compareTag(reference, document)) {
+      val referenceChildren = reference.children().asScala
+      val documentChildren = document.children().asScala
+      if (referenceChildren.length != documentChildren.length) {
+        mismatch = LengthMismatch :: mismatch
+      }
+
+      compareText(reference, document) match {
+        case Some(textMismatch) =>
+          if (referenceChildren.isEmpty && documentChildren.isEmpty) {
+            logger.info(s"Wrapper: ${reference.text()}")
+            logger.info(s"Document: ${document.text()}")
+          }
+          mismatch = textMismatch :: mismatch
+        case None =>
+      }
+
+      mismatch
+    } else {
+      TagMismatch :: mismatch
+    }
+  }
+
+  def zipChildren(referenceChildren: List[Element], documentChildren: List[Element]): List[(Option[Element], Option[Element])] = {
+    (referenceChildren, documentChildren) match {
+      case (List(), List()) => Nil
+      case (List(), documentHead::documentTail) => (None, Some(documentHead)) :: zipChildren(Nil, documentTail)
+      case (wrapperHead::wrapperTail, List()) => (Some(wrapperHead), None) :: zipChildren(Nil, wrapperTail)
+      case (wrapperHead::wrapperTail, documentHead::documentTail) =>
+        if (compareTag(wrapperHead, documentHead)) {
+          (Some(wrapperHead), Some(documentHead)) :: zipChildren(wrapperTail, documentTail)
+        } else if(referenceChildren.length >= documentChildren.length) {
+          (Some(wrapperHead), None) :: zipChildren(wrapperTail, documentHead::documentTail)
+        } else {
+          (None, Some(documentHead)) :: zipChildren(wrapperHead::wrapperTail, documentTail)
+        }
+    }
+  }
+
+  private def expandWrapper(rootReference: Element, rootDocument: Element): Element = {
     // TODO:
     //    1. iterate over tags
     //    2. solve mismatches:
     //      - text mismatches -> data fields OR items
     //      - tag mismatches -> optional items OR iterators (list of items)
-    var mismatch = false
-    val wrapperChildren = rootWrapper.children().asScala
-    val documentChildren = rootDocument.children().asScala
-    if (wrapperChildren.length != documentChildren.length) {
-//      logger.info(s"Children length mismatch")
-      mismatch = true
-    }
+    val htmlMismatches: List[Mismatch] = compareNodes(rootReference, rootDocument)
 
-    if (wrapperChildren.isEmpty && documentChildren.isEmpty) {
-      handleEmptyChildren(rootWrapper, rootDocument)
-    }
+    val referenceChildren: List[Element] = rootReference.children().asScala.toList
+    val documentChildren: List[Element] = rootDocument.children().asScala.toList
 
-    val zipped = wrapperChildren.zipAll(documentChildren, None, None)
+//    if (htmlMismatches.nonEmpty) logger.info(s"Mismatch types: $htmlMismatches")
+
+    val zipped = zipChildren(referenceChildren, documentChildren)
+//    logger.info(s"Zipped: ${zipped.map(el => s"(${el._1.map(_.tagName())}, ${el._2.map(_.tagName())})")}")
     zipped.foreach {
-      case (wrapper: Element, document: Element) =>
+      case (Some(wrapper: Element), Some(document: Element)) =>
         expandWrapper(wrapper, document)
-      case (None, document: Element) =>
-//        logger.info(s"Missing wrapper, mismatch: $mismatch")
+      case (None, Some(document: Element)) =>
+        // TODO: mark element as optional and add it to the wrapper
+        logger.info(s"Missing wrapper, htmlMismatches: $htmlMismatches")
 //        logger.info(s"$document") // is optional
-      case (wrapper: Element, None) =>
-//        logger.info(s"Missing document, mismatch: $mismatch")
+      case (Some(wrapper: Element), None) =>
+        // TODO: mark element as optional
+        logger.info(s"Missing document, htmlMismatches: $htmlMismatches")
 //        logger.info(s"$wrapper") // is optional
       case rest => logger.error(s"wut? $rest")
     }
 
-    rootWrapper
+    rootReference
   }
 }
