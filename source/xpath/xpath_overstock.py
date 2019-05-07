6f2eb9bb0cc2b758d47d19484a1986bfd59a4e9b
Gojko <hajdukovicgojko3@gmail.com>
2019-05-07 18:01:52 +0200

    almost

--- source/xpath/xpath_overstock.py
+++ source/xpath/xpath_overstock.py
@@ -12,7 +12,7 @@ saving = [i.split(" ") for i in tree.xpath('//td[b[text()="You Save:"]]/followin
 content = tree.xpath('//td[span[@class="normal"]]/span/text()')
 allproducts = list(zip(pName,listprice,price,saving,content))
 
-items = {'Item_{}'.format(i+1):{'Title':allproducts[i][0],'ListPrice':allproducts[i][1],'Price':allproducts[i][2],'Savings':(allproducts[i][3])[0],'SavingsPercent':(allproducts[i][3])[1].strip('()'),'Content':allproducts[i][4].replace('\n','')} for i in range(0,len(allproducts))}
+items = [{'Title':allproducts[i][0],'ListPrice':allproducts[i][1],'Price':allproducts[i][2],'Saving':(allproducts[i][3])[0],'SavingPercent':(allproducts[i][3])[1].strip('()'),'Content':allproducts[i][4].replace('\n','')} for i in range(0,len(allproducts))]
 
 output = sys.argv[2]
 out = open(output , 'w').write(json.dumps(items,indent=4,ensure_ascii=False))
